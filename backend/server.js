a459e06efc69508ce592b34c2f34dca104449b71
RajRajendram <72311278+RajRajendram@users.noreply.github.com>
2024-10-04 10:28:42 -0400

    Register Integration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const {connectDB} = require('./config/db');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
@@ -9,6 +10,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+
+//Use the cors middleware to allow cors-origin request
+app.use(cors());
 
 //Routes
 app.use('/api/v1/users', userRoutes);
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,41 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+import axiosInstance from '../api/api';
 
-function Register() {
+const  Register = () => {
+  const [formData, setFormData] = useState({
+    name:'',
+    email:'',
+    phone:'',
+    city:'',
+    password:'',
+    confirmPassword:''
+  })
+
+  const handleSumit = async (e) => {
+    e.preventDefault();
+
+    if(formData.password !== formData.confirmPassword){
+      toast.error('Password do not match');
+      return;
+    }
+    try{
+      await axiosInstance.post('/user/register', formData);
+      toast.success('Registration sucessfull');
+    }catch(error){
+      toast.error(error);
+    }
+  }
+
   return (
     <>
     <h2>Register Here</h2>
 
+    <form onSubmit={handleSumit}>
     <input
     type='text'
     placeholder='Enter your name'
+    value={formData.name}
+    onChange={(e) => setFormData({...formData, name:e.target.value})}
     />
     <br/>
     <input
     type='email'
     placeholder='Enter your email'
+    value={formData.email}
+    onChange={(e) => setFormData({...formData, email:e.target.value})}
     />
     <br/>
     <input
     type='phone'
     placeholder='Enter your phone number'
+    value={formData.phone}
+    onChange={(e) => setFormData({...formData, phone:e.target.value})}
     />
     <br/>
     <input
     type='text'
     placeholder='Enter your city'
+    value={formData.city}
+    onChange={(e) => setFormData({...formData, city:e.target.value})}
     />
     <br/>
     <input
     type='Password'
     placeholder='Enter your password'
+    value={formData.password}
+    onChange={(e) => setFormData({...formData, password:e.target.value})}
     />
     <br/>
     <input
     type='password'
     placeholder='Confirm your password'
+    value={formData.confirmPassword}
+    onChange={(e) => setFormData({...formData, confirmPassword:e.target.value})}
     />
     <br/>
-    <button>Register</button>
+    <button type='submit'>Register</button>
+    </form>
 
     </>
     
